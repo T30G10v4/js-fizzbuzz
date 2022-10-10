5f7c52ba15b34f2a94a63c4a2ced0d92c7b48add
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-10-10 13:33:04 +0200

    Tolta dichiarazione fuori for

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@
 //  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 //  Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
 // Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
-let append = "";
+
 
 for(let i = 1; i<=100; i++) {
 
